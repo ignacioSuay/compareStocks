c09d61472de6d2fabb272f3e466d080f9af27ad7
Ignacio Suay <ignacio.suay@gmail.com>
2015-06-18 17:17:20 +0100

    load data file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,19 @@
  * Controller of the compareStocksApp
  */
 angular.module('compareStocksApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('data/sb.json')
+      .then(function(res){
+        $scope.data = res.data;
+        console.log("everything is loaded fine");
+        console.log($scope.data.length);
+
+      });
+
   });
