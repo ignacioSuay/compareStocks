a9307c6cc20787dc6e968a020cc0237abf162935
Ignacio Suay <ignacio.suay@gmail.com>
2015-06-18 16:30:33 +0100

    create compareStocks project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name compareStocksApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the compareStocksApp
+ */
+angular.module('compareStocksApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name compareStocksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the compareStocksApp
+ */
+angular.module('compareStocksApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
